6f68a6da1df50a074d0144853dd0e8f294324a5b
garyh6 <hsiaog6@gmail.com>
2016-06-27 14:11:45 -0700

    New Login and Registration

--- apps/dash/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from . import views # This line is new!
-
-urlpatterns = [
-  url(r'^$', views.dashboard),
-  url(r'^admin/', views.admin),
-]
