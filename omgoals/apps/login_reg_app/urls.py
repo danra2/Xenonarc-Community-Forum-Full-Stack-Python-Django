b6b2e64bb3ae1ab557eb951b008ecbd12600c5b8
garyh6 <hsiaog6@gmail.com>
2016-06-27 15:38:01 -0700

    Login and reg with flash

---
+++ omgoals/apps/login_reg_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^signin$', views.signin, name='signin'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^validate$', views.validation, name='validation'),
+    url(r'^success$', views.success, name='success')
+]
