251d7656ef971df6177b06ddfe3042f02202b43b
kailock <kailock@uw.edu>
2016-06-29 21:41:57 -0700

    jquery to add page

---
+++ omgoals/apps/login_reg_app/static/js/add_goal.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    //Function to hide all animal pictures
+   (function ( $ ) {
+        $.fn.hideAnimals = function() {
+            $("#penguin").hide();
+            $("#horse").hide();
+            $("#owl").hide();
+        }
+    })( jQuery ); 
+    $("#animals").hideAnimals();
+
+    //Display animal picture with corresponding category
+    $("#category").change(function() {
+        $("#animals").hideAnimals();
+        if ($(this).val() === 'career') {
+            $("#penguin").show();
+        } else if ($(this).val() === 'spiritual') {
+            $("#horse").show();
+        } else if ($(this).val() === 'education') {
+            $("#owl").show();
+        }        
+    })
+});
+
