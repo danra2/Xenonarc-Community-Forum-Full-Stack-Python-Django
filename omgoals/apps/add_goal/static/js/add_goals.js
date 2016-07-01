0580e6f3a30aa4cf6b92ddb56fded46e32a301a0
ghyoun <ghyoun@uw.edu>
2016-06-30 17:58:55 -0700

    added a add milestone button

--- omgoals/apps/add_goal/static/js/add_goals.js
+++ omgoals/apps/add_goal/static/js/add_goals.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     //Function to hide all animal pictures
+    var milestoneNum = 1;
+
    (function ( $ ) {
         $.fn.hideAnimals = function() {
             $("#penguin").hide();
@@ -23,6 +25,16 @@ $(document).ready(function(){
             $("#owl").show();
             $("#animal_name").val("Owl");
         }
-    })
+    });
 
+    $("#plus").click(function() {
+        if (milestoneNum < 5) {
+            milestoneNum++;
+            $('#milestone_insert').before('<div class="form-group"><label for="milestone'+milestoneNum+'" class="col-sm-2 control-label">Milestone #'+milestoneNum+'</label><div class="col-sm-9"><input class="form-control milestone" type="text" placeholder="Ex: Run 2 laps around the track" name="milestone'+milestoneNum+'" /></div>');
+        }
+        if (milestoneNum === 5) {
+            $('#plus').hide();
+        }
+
+    });
 });
